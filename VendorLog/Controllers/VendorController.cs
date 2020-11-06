28a6f42df0a874206cd6cc7a59b91d56955f69da
Tristen Everett <spock214@gmail.com>
2020-11-06 11:34:44 -0800

    create page and controller to display vendors

---
+++ VendorLog/Controllers/VendorController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorLog.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendor")]
+    public ActionResult Index()
+    {
+      List<Vendor> vendorList = Vendor.GetAll();
+      return View();
+    }
+  }
+}
