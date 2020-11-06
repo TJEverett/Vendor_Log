9ab2bb7e5b213da78ada65b291c2d33a24b944ee
Tristen Everett <spock214@gmail.com>
2020-11-06 10:39:57 -0800

    create a basic layout to be used for each webpage

--- VendorLog/Startup.cs
+++ VendorLog/Startup.cs
@@ -25,6 +25,8 @@ namespace VendorLog
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseStaticFiles();
+
       app.UseDeveloperExceptionPage();
 
       app.UseMvc(routes =>
