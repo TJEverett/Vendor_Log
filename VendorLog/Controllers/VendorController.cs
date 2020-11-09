1b0a1a3a5f74f2bdf093be82fcfac8e67ebeb2b3
Tristen Everett <spock214@gmail.com>
2020-11-09 10:23:52 -0800

    create post route to create a vendor object and a get route to view vendor objects

--- VendorLog/Controllers/VendorController.cs
+++ VendorLog/Controllers/VendorController.cs
@@ -19,5 +19,28 @@ namespace MusicOrganizer.Controllers
     {
       return View();
     }
+
+    [HttpPost("/vendor")]
+    public ActionResult Create(string name, string description)
+    {
+      if (String.IsNullOrWhiteSpace(description))
+      {
+        description = "No Description Set";
+      }
+
+      if (!(String.IsNullOrWhiteSpace(name)))
+      {
+        Vendor newVendor = new Vendor(name, description);
+      }
+
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendor/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor foundVendor = Vendor.Find(id);
+      return View(foundVendor);
+    }
   }
 }
